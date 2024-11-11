123e6d454d43730671750533a64920a332123e37
Jeswin Jose <jeswin.j@thinkpalm.com>
2024-11-11 11:56:00 +0530

    abc

--- bake/src/Side.js
+++ bake/src/Side.js
@@ -1,11 +1,47 @@
-
-import { Layout } from "antd";
-const {Sider} = Layout;
-
-function Side() {
+import './App.css';
+import { Layout, Menu, theme } from "antd";
+import ReactDOM from "react-dom/client";
+import React from "react";
+ 
+const { Sider } = Layout;
+const { SubMenu } = Menu; // Import SubMenu from Menu
+ 
+const Side = () => {
+  const {
+    token: { colorBgContainer }
+  } = theme.useToken();
+ 
   return (
-          <Sider>Left Sider</Sider>
+    <Layout style={{ minHeight: '100vh' }}>
+      <Sider
+        breakpoint="lg"
+        collapsedWidth="0"
+        onBreakpoint={(broken) => {
+          console.log(broken);
+        }}
+        onCollapse={(collapsed, type) => {
+          console.log(collapsed, type);
+        }}
+      >
+        <div className="demo-logo-vertical" />
+        <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+          <Menu.Item key="1">Home</Menu.Item>
+          <SubMenu key="sub1" title="About">
+            <Menu.Item key="2">Our Team</Menu.Item>
+            <Menu.Item key="3">Our Story</Menu.Item>
+          </SubMenu>
+          <SubMenu key="sub2" title="Services">
+            <Menu.Item key="4">Consulting</Menu.Item>
+            <Menu.Item key="5">Development</Menu.Item>
+            <Menu.Item key="6">Support</Menu.Item>
+          </SubMenu>
+          <Menu.Item key="7">Contact</Menu.Item>
+        </Menu>
+      </Sider>
+    </Layout>
   );
-}
-
+};
+ 
 export default Side;
+ 
+ 
